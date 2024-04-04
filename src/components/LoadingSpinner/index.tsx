31fdf9b3e6bd20f988e84eb204baf76b01674bb7
chldmswnl <chldmswnl12@gmail.com>
2024-04-04 17:43:29 +0900

    feat: LoadingSpinner component 추가

---
+++ src/components/LoadingSpinner/index.tsx
@@ -0,0 +1,7 @@
+export default function LoadingSpinner() {
+	return (
+		<div className="w-full h-full flex justify-center items-center">
+			<img alt="loading" src="/loading-spinner.svg" />
+		</div>
+	);
+}
