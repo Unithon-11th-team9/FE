2dcb89ae00bd5616a8714305ac4249312cdde30d
chldmswnl <chldmswnl12@gmail.com>
2024-04-06 21:20:50 +0900

    feat: Button component 수정

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -10,10 +10,10 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 type ButtonType = "button1" | "button2" | "button2_off" | "button3" | "button4";
 
 const buttonStyles = {
-	button1: " rounded-button text-white bg-point text-mainText p-button hover:bg-point/70",
-	button2: "rounded-button text-white bg-point text-mainText p-button hover:bg-point/70",
+	button1: " rounded-button text-white bg-point text-xl p-button hover:bg-point/70",
+	button2: "rounded-button text-white bg-point text-xl p-button hover:bg-point/70",
 	button2_off: "rounded-button text-white/50 bg-back03 text-mainText p-button hover:bg-back03/70",
-	button3: "rounded-button text-point bg-white text-mainText p-button hover:bg-back03",
+	button3: "rounded-button text-point bg-white text-xl p-button hover:bg-point hover:text-white",
 	button4: "rounded-button text-white bg-point text-subText p-button_sm hover:bg-point/70",
 };
 
