a5e7b0f78a98111e86bbab6bb9315290bfa23cc5
chldmswnl <chldmswnl12@gmail.com>
2024-04-06 15:28:53 +0900

    feat: Button hover 추가

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -9,11 +9,11 @@ type ButtonType = "button1" | "button2" | "button2_off" | "button3" | "button4";
 
 const defaultStyle = "rounded-button";
 const buttonStyles = {
-	button1: "text-white bg-point text-mainText p-button",
-	button2: "text-white bg-point text-mainText p-button",
-	button2_off: "text-white/50 bg-back03 text-mainText p-button",
-	button3: "text-point bg-white text-mainText p-button",
-	button4: "text-white bg-point text-subText p-button_sm",
+	button1: "text-white bg-point text-mainText p-button hover:bg-point/70",
+	button2: "text-white bg-point text-mainText p-button hover:bg-point/70",
+	button2_off: "text-white/50 bg-back03 text-mainText p-button hover:bg-back03/70",
+	button3: "text-point bg-white text-mainText p-button hover:bg-back03",
+	button4: "text-white bg-point text-subText p-button_sm hover:bg-point/70",
 };
 
 export default function Button({ buttonType, children, ...props }: ButtonProps) {
