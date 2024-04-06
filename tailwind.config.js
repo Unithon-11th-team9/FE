238895af04e917357644a8929eb665422f41170e
bm4656 <newjin46@gmail.com>
2024-04-06 12:48:24 +0900

    design: 메인 컬러 테일윈드에 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {
+			colors: {
+				point: "#13D20C",
+				back01: "#17073A",
+				back02: "#202020",
+				white: "#FFFFFF",
+				black: "#000000",
+			},
+		},
+	},
+	plugins: [],
 };
