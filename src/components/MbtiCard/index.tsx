06ade61dd011a69cf322dd7726ea4742bf7598fc
bm4656 <newjin46@gmail.com>
2024-04-06 19:43:29 +0900

    feat: MbtiCard 변수, 타입 추가

--- src/components/MbtiCard/index.tsx
+++ src/components/MbtiCard/index.tsx
@@ -1,11 +1,16 @@
-export default function MbtiCard() {
+type MbtiCardProps = {
+	name: string;
+	type: "E" | "I" | "F" | "T";
+};
+
+export default function MbtiCard({ name, type }: MbtiCardProps) {
 	return (
 		<article className="w-[158px] h-[119px]rounded-[30px] flex flex-col justify-center items-center text-xl gap-[0.1rem]">
 			<h1>
-				너 <span className="text-point">E</span>야?
+				너 <span className="text-point">{type}</span>야?
 			</h1>
 			<p className="text-xs">말을 가장 많이한 사람</p>
-			<p className="text-point mt-1">김민지</p>
+			<p className="text-point mt-1">{name}</p>
 		</article>
 	);
 }
