70f160e0134af7ce3002a360e497b9bc8b722d03
chldmswnl <chldmswnl12@gmail.com>
2024-04-07 05:49:48 +0900

    chore: 도메인 변경

--- src/apis/axiosInstance.ts
+++ src/apis/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-	baseURL: "https://ttobot.kro.kr/",
+	baseURL: "https://peacetalk.co.kr",
 	headers: { "Content-Type": "application/json" },
 });
 
