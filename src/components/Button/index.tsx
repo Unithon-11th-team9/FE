6a02010394cad229388e06499df35e3aff02d27e
chldmswnl <chldmswnl12@gmail.com>
2024-04-06 15:14:05 +0900

    feat: Button 컴포넌트 추가

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,12 +1,24 @@
 import React from "react";
 
-type ButtonProps = {
-	children: React.ReactNode;
-} & React.ButtonHTMLAttributes<HTMLButtonElement>;
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+	buttonType: ButtonType;
+	children: string;
+}
 
-export default function Button({ children, ...props }: ButtonProps) {
+type ButtonType = "button1" | "button2" | "button2_off" | "button3" | "button4";
+
+const defaultStyle = "rounded-button";
+const buttonStyles = {
+	button1: "text-white bg-point text-mainText p-button",
+	button2: "text-white bg-point text-mainText p-button",
+	button2_off: "text-white/50 bg-back03 text-mainText p-button",
+	button3: "text-point bg-white text-mainText p-button",
+	button4: "text-white bg-point text-subText p-button_sm",
+};
+
+export default function Button({ buttonType, children, ...props }: ButtonProps) {
 	return (
-		<button {...props}>
+		<button className={`${defaultStyle} ${buttonStyles[buttonType]}`} {...props}>
 			<span>{children}</span>
 		</button>
 	);
