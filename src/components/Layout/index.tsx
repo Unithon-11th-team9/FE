b637b2f2d16aec9d69f1712093b4f54517dbfbf0
chldmswnl <chldmswnl12@gmail.com>
2024-04-06 17:51:20 +0900

    feat: tailwind merge 라이브러리 설치

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,14 +1,10 @@
 import { twMerge } from "tailwind-merge";
 
-interface LayoutProps extends React.HTMLAttributes<HTMLDivElement> {
+interface LayoutProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 	children: React.ReactNode;
 	classNames?: string;
 }
 
-export default function Layout({ children, classNames, ...props }: LayoutProps) {
-	return (
-		<section className={twMerge("w-mobile bg-black h-full", classNames)} {...props}>
-			{children}
-		</section>
-	);
+export default function Layout({ children, classNames }: LayoutProps) {
+	return <section className={twMerge("w-mobile bg-black h-full", classNames)}>{children}</section>;
 }
