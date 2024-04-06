18efdd2d8dcb23c255385fef5a623c9fb7ea73c3
chldmswnl <chldmswnl12@gmail.com>
2024-04-06 15:17:16 +0900

    feat: Typography 컴포넌트 추가

---
+++ src/components/Typography/index.tsx
@@ -0,0 +1,31 @@
+type TextName =
+	| "title_01"
+	| "title_02"
+	| "name_text_01"
+	| "main_text_01"
+	| "main_text_02"
+	| "main_text_03"
+	| "sub_text_01"
+	| "sub_text_02"
+	| "sub_text_03";
+
+type TextProps = {
+	type: TextName;
+	children: string;
+};
+
+const textStyles = {
+	title_01: "text-white font-['PyeongChangPeace-Bold'] text-title01",
+	title_02: "text-white font-['PyeongChangPeace-Bold'] text-title02",
+	name_text_01: "text-white font-['PyeongChangPeace-Bold'] text-nameText01 leading-nameText01",
+	main_text_01: "text-white text-mainText leading-5",
+	main_text_02: "text-white text-mainText",
+	main_text_03: "text-white text-mainText leading-mainText03",
+	sub_text_01: "text-white text-subText leading-subText01",
+	sub_text_02: "text-white text-subText leading-subText02",
+	sub_text_03: "text-white text-subText leading-subText03",
+};
+
+export default function Typography({ type, children }: TextProps) {
+	return <span className={textStyles[type]}>{children}</span>;
+}
