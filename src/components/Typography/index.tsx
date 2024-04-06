57a6b766015b215be00315b72128b225ba15d810
chldmswnl <chldmswnl12@gmail.com>
2024-04-06 21:21:03 +0900

    feat: Typography 컴포넌트 수정

--- src/components/Typography/index.tsx
+++ src/components/Typography/index.tsx
@@ -18,15 +18,15 @@ type TextProps = {
 };
 
 const textStyles = {
-	title_01: "text-white font-['PyeongChangPeace-Bold'] text-title01",
-	title_02: "text-white font-['PyeongChangPeace-Bold'] text-title02",
+	title_01: "text-white font-['PyeongChangPeace-Bold'] text-4xl",
+	title_02: "text-white font-['PyeongChangPeace-Bold'] text-4xl",
 	name_text_01: "text-white font-['PyeongChangPeace-Bold'] text-nameText01 leading-nameText01",
 	main_text_01: "text-white text-mainText leading-5",
 	main_text_02: "text-white text-mainText",
 	main_text_03: "text-white text-mainText leading-mainText03",
-	sub_text_01: "text-white text-subText leading-subText01",
-	sub_text_02: "text-white text-subText leading-subText02",
-	sub_text_03: "text-white text-subText leading-subText03 font-[SBAggroL]",
+	sub_text_01: "text-white text-xs leading-subText01",
+	sub_text_02: "text-white text-xs leading-subText02",
+	sub_text_03: "text-white text-xs leading-subText03 font-[SBAggroL]",
 };
 
 export default function Typography({ type, classNames, children }: TextProps) {
