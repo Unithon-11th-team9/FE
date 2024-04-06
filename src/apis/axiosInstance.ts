918133313cf5f7a35409587e053d7965f0744f2b
chldmswnl <chldmswnl12@gmail.com>
2024-04-07 05:45:06 +0900

    chore: 디자인 수정

--- src/apis/axiosInstance.ts
+++ src/apis/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-	baseURL: "http://3.36.114.87:8000",
+	baseURL: "https://ttobot.kro.kr/",
 	headers: { "Content-Type": "application/json" },
 });
 
--- src/pages/JoinPage/index.tsx
+++ src/pages/JoinPage/index.tsx
@@ -21,9 +21,15 @@ export default function JoinPage() {
 				</Button>
 			</section>
 			<section className="w-full flex justify-center text-center pt-6 pb-6">
-				<Typography type="sub_text_01" classNames="whitespace-pre-wrap">
-					{`실제 카톡 대화를 보고 AI가 심사해드립니다.\n대화 내용은 사람을 거치지 않고, 분석 후 즉시 자동 폐기됩니다.\n대화 분석을 위한 용도 이외에는 전혀 사용되지 않습니다.`}
-				</Typography>
+				<div>
+					<Typography type="sub_text_01">{`실제 카톡 대화를 보고 `}</Typography>
+					<Typography type="sub_text_01" classNames="whitespace-pre-wrap text-point">
+						{`인공지능(AI)이 심사`}
+					</Typography>
+					<Typography type="sub_text_01" classNames="whitespace-pre-wrap">
+						{`해드립니다.\n대화 내용은 사람을 거치지 않고, 분석 후 즉시 자동 폐기됩니다.\n서비스 제공을 위한 용도 이외로 사용되지 않습니다.`}
+					</Typography>
+				</div>
 			</section>
 		</Layout>
 	);
